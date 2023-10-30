688867af8f67dec67a17f485539f1bcf3010550b
Harsh Amin <akaharsh05@hotmail.com>
2023-10-30 17:02:49 -0400

    added user specific implementation using cookies

--- models/zipurl.js
+++ models/zipurl.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose')
-const shortId = require('shortid')
-
+const mongoose = require('mongoose');
+const shortId = require('shortid');
 
 const zipUrlSchema = new mongoose.Schema({
-    full: {
-        type: String,
-        required: true
-       },
-    short: {
-        type: String,
-        required: true,
-        default: shortId.generate().substring(0,5)
-    },
-    clicks: {
-        type: Number,
-        required: true,
-        default: 0
-    }
-})
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate().substring(0, 5),
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  // Add a user identifier field to associate URLs with users
+  userIdentifier: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('zipurl', zipUrlSchema)
+module.exports = mongoose.model('zipurl', zipUrlSchema);
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ZipUrl = require('./models/zipurl')
+const express = require('express');
+const mongoose = require('mongoose');
+const ZipUrl = require('./models/zipurl');
+const cookieParser = require('cookie-parser'); // Require cookie-parser
 
 require('dotenv').config();
 const uri = process.env.MONGODB_URI;
 
-const app = express()
+const app = express();
 
 mongoose.connect(uri, {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 
 // Handle successful connection
@@ -17,30 +19,50 @@ db.on('connected', () => {
   console.log('Connected to MongoDB successfully');
 });
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+// Use the cookie-parser middleware
+app.use(cookieParser());
+
+// Middleware to identify users using cookies
+app.use((req, res, next) => {
+  // Check if the user has a unique identifier cookie, if not, create one
+  if (!req.cookies.userIdentifier) {
+    const userIdentifier = 'user_' + Date.now(); // Generate a unique user identifier
+    res.cookie('userIdentifier', userIdentifier, { maxAge: 900000 }); // Set a cookie with a 15-minute expiration
+    req.userIdentifier = userIdentifier;
+  } else {
+    req.userIdentifier = req.cookies.userIdentifier;
+  }
+
+  next();
+});
 
 app.get('/', async (req, res) => {
-  const zipurls = await ZipUrl.find()
-  res.render('index', { zipurls: zipurls })
-})
-
-
+  // Retrieve data specific to the user
+  const zipurls = await ZipUrl.find({ userIdentifier: req.userIdentifier });
+  res.render('index', { zipurls: zipurls });
+});
 
 app.get('/:zipurl', async (req, res) => {
-  const zip = await ZipUrl.findOne({ short: req.params.zipurl })
-  if (zip == null) return res.sendStatus(404)
+  const zip = await ZipUrl.findOne({ short: req.params.zipurl, userIdentifier: req.userIdentifier });
+  if (zip == null) return res.sendStatus(404);
 
-  zip.clicks++
-  zip.save()
+  zip.clicks++;
+  zip.save();
 
-  res.redirect(zip.full)
-})
+  res.redirect(zip.full);
+});
 
 app.post('/zipurl', async (req, res) => {
-    await ZipUrl.create({ full: req.body.fullUrl })
-  
-    res.redirect('/')
-  })
+  // Get the user identifier from the cookie
+  const userIdentifier = req.userIdentifier;
+
+  // Create data associated with the user
+  await ZipUrl.create({ full: req.body.fullUrl, userIdentifier: userIdentifier });
+
+  res.redirect('/');
+});
 
 app.listen(process.env.PORT || 5000);
